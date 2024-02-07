6686cb1b1d35fe9a6237a09fde09ac038dce7ac9
mo7386 <73697312+mo7386@users.noreply.github.com>
2024-02-07 12:06:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['967712272493', '🌩️ ẉa.me//𝑀𝐸𝐺𝛩‖.peị!‽ 👑', true]]
+global.owner = [['967712272493', '🌩️ ẉa.me//ɴεƶσ‖.peị!‽ 👑', true]]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
