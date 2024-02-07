45f5ca8aa09c5072babd56afb6a9fbc2f6797f58
mo7386 <73697312+mo7386@users.noreply.github.com>
2024-02-07 12:04:07 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,82 +1,37 @@
 import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
-import { fileURLToPath } from 'url'
-import fs from 'fs'
-import fetch from 'node-fetch'
-import axios from 'axios'
+import { fileURLToPath, pathToFileURL } from 'url'
 
+global.owner = [['967712272493', '🌩️ ẉa.me//𝑀𝐸𝐺𝛩‖.peị!‽ 👑', true]]
 
-global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
-  [''] 
-] //Number of owners
+//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
+//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
+global.botNumberCode = '' //Ejemplo: +59309090909
+global.confirmCode = ''
 
-//global.pairingNumber = "" //put your bot number here
+global.animxscans = ['967712272493']
+global.suittag = ['967712272493']
+global.mods = []
+global.prems = []
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
-global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
-global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
-global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
-global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
-global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
-global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
-global.lolkeysapi = ['GataDios']
+global.packname = '『 نيزو 』'
+global.author = '『 نيزوكو بــ💃ـوت 』'
+global.wm = 'نيزوؤؤ بــ🥀ـؤؤت 』'
+global.wm2 = '『 نيزو بــ💎ـوت 』'
+global.azami = '『نيزؤؤ بــ💞ـؤؤ 』'
+global.cb = '『 نيزو بــ👾ـوت 』'
 
-global.APIs = { // API Prefix
-  // name: 'https://website'
-  xteam: 'https://api.xteam.xyz', 
-  dzx: 'https://api.dhamzxploit.my.id',
-  lol: 'https://api.lolhuman.xyz',
-  violetics: 'https://violetics.pw',
-  neoxr: 'https://api.neoxr.my.id',
-  zenzapis: 'https://zenzapis.xyz',
-  akuari: 'https://api.akuari.my.id',
-  akuari2: 'https://apimu.my.id',
-  nrtm: 'https://fg-nrtm.ddns.net',
-  bg: 'http://bochil.ddns.net',
-  fgmods: 'https://api-fgmods.ddns.net'
-}
-global.APIKeys = { // APIKey Here
-  // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
-  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-  'https://api.neoxr.my.id': `${keysneoxr}`,	
-  'https://violetics.pw': 'beta',
-  'https://zenzapis.xyz': `${keysxxx}`, 
-  'https://api-fgmods.ddns.net': 'fg-dylux'
-}
-
-// Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
-global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
-
-
-global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
-global.rwait = '⌛'
-global.dmoji = '🤭'
-global.done = '✅'
-global.error = '❌' 
-global.xmoji = '🔥' 
-
-global.multiplier = 69 
-global.maxwarn = '3' 
+global.vs = 'V2 • 1.0.5'
+global.library = 'Baileys'
+global.baileys = '@whiskeysockets/baileys'
+global.lenguaje = 'Español'
+global.KMA = '╰━━━〔 *🛡️ 1.7.9* 〕━━━━━⬣'
+global.menudi = ['⛶','❏','⫹⫺']
+global.dev = '© Nezu_BOT'
+global.devnum = '+967712272493'
 
 let file = fileURLToPath(import.meta.url)
-watchFile(file, () => {
-  unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
-  import(`${file}?update=${Date.now()}`)
+watchFile(file, () => { unwatchFile(file)
+console.log(chalk.yellow('Se actualizo el archivo config.js'))
+import(`${file}?update=${Date.now()}`)
 })
